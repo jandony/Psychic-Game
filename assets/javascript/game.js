97e981c9aa80fe8db3e2634469567fcf1ffc20b3
Jeff Andony <jeff.andony@gmail.com>
2019-06-01 12:46:40 -0600

    Made reset function, cleaned up if/else statements, and created an array to fill in for each user guess.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,10 @@
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var guessedLetters = [];
 
 // array of potential computer choices (if placed inside the function, numbers will reset each time)
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var userChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // references to all the html sections we want to update (making shortcuts)
 var directionsText = document.getElementById("directions");
@@ -13,51 +13,57 @@ var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
 var guessesLeftText = document.getElementById("guessesLeft");
 var userGuessText = document.getElementById("userGuess");
-var computerGuessText = document.getElementById("computerGuess");
 
-// computer thinks of a number
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(computerGuess)
+
+alert("Click ok to start!");
 
 // create a reset function
 function reset() {
     // updating the HTML to reflect the new variable values
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(computerGuess)
+    guessesLeft = 9;
+    guessedLetters = [];
+}
+
+function returnArray() {
+    for (i = 0; i < guessedLetters.length; i++) {
+        var result = "" ;
+
+        var newLetter = guessedLetters[i];
+        if (i !== guessedLetters.length -1) {
+            str.concat(newLetter, ", ");
+        }
+
+    }
+}
+
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+
+    // how you win or lose
+    if (userGuess === computerGuess) {
+        wins++;
+        reset();
+    } else {
+        guessesLeft--;
+        guessedLetters.push(userGuess);
+    
+        console.log(guessedLetters)
+        userGuessText.textContent = returnArray();
+    }
+
+    if (guessesLeft == 0) {
+        losses++;
+        reset();
+    }
+
     directionsText.textContent = "";
     winsText.textContent = wins;
     lossesText.textContent = losses;
     guessesLeftText.textContent = guessesLeft;
-    userGuessText.textContent = userGuess;
-    computerGuessText.textContent = computerGuess;
-}
-
-// 1 - User presses key
-// 2 - Computer selects a letter
-// 3 - If user key is equal to computer letter, wins adds 1 pt
-// 4 - If user does not equal to computer letter, losses adds 1 pt 
-// 5 - Every round has 9 attempts 
-// 6 - If 9 attemps is equal to 0, reset game
-
-document.onkeyup = function (event) {
-    var userGuess = event.key;
-    console.log("Computer's guess: " + computerGuess);
-    console.log("User's guess:" + userGuess);
+    userGuessText.textContent = returnArray();
     
-        if (userGuess) {
-
-            // game reset
-            if (guessesLeft === 0 || wins === 9  || losses === 9) {
-                reset();
-            } else {
-                guessesLeft--;
-            }
-            
-            // how you win or lose
-            if (userGuess === computerGuess) {
-                wins++;
-            } else {
-                losses++;
-            }
-
-        } else {
-            alert("Press a key that corresponds with the alphabet!");
-        }
-    }
+}
